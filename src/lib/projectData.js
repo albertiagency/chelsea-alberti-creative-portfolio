352e1a51818377864a3b2ac2c4f24a588d81f446
Chelsea Alberti <chelseaalberti@gmail.com>
2026-06-18 18:13:30 +0000

    File changes

--- src/lib/projectData.js
+++ src/lib/projectData.js
@@ -8,7 +8,7 @@ export const projectData = {
       deliverables: 'Logo, brand guidelines, business cards, yard signage, vehicle graphics, full website design & build',
       category: 'Brand & Marketing',
       images: [
-        'https://media.base44.com/images/public/6a3416a0b2d81e218daa4cbb/78e48d500_Screenshot2026-06-18at123209PM.png',
+        'https://media.base44.com/images/public/6a3416a0b2d81e218daa4cbb/3d985562e_Screenshot2026-06-18at21218PM.png',
         'https://media.base44.com/images/public/6a3416a0b2d81e218daa4cbb/dfdec04e6_generated_c1ad4483.png',
       ],
     },
