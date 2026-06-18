82fca1c2c5d1bc352170d3c9429b7cd6f9030a1d
Chelsea Alberti <chelseaalberti@gmail.com>
2026-06-18 17:22:05 +0000

    File changes

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,7 +3,7 @@ import { motion } from 'framer-motion';
 import { MapPin } from 'lucide-react';
 import TestimonialCard from '@/components/about/TestimonialCard';
 
-const ABOUT_PORTRAIT = 'https://media.base44.com/images/public/6a3416a0b2d81e218daa4cbb/b8a2065bf_20250914_Chelsea_Studio_10.jpg';
+const ABOUT_PORTRAIT = 'https://media.base44.com/images/public/6a3416a0b2d81e218daa4cbb/fab71641c_20250914_Chelsea_Studio_15.jpg';
 
 const journey = [
   {
