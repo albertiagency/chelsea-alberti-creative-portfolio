d5fa4fd4a612ca1635f90c0e029c824346fe0e83
Chelsea Alberti <chelseaalberti@gmail.com>
2026-06-19 16:33:31 +0000

    File changes

--- src/lib/projectData.js
+++ src/lib/projectData.js
@@ -125,6 +125,7 @@ export const projectData = {
       category: 'Creative Production',
       images: [
         'https://media.base44.com/images/public/6a3416a0b2d81e218daa4cbb/d598eb895_Screenshot2026-06-18at12814PM.png',
+        'https://media.base44.com/images/public/6a3416a0b2d81e218daa4cbb/a8026f738_Screenshot2026-06-19at122514PM.png',
         'https://media.base44.com/images/public/6a3416a0b2d81e218daa4cbb/a4e077dae_Screenshot2025-02-23at61452PM.png',
         'https://media.base44.com/images/public/6a3416a0b2d81e218daa4cbb/02afb6b40_Screenshot2026-06-19at123208PM.png',
         'https://media.base44.com/images/public/6a3416a0b2d81e218daa4cbb/74b05f16f_Screenshot2025-02-24at60129PM.png',
